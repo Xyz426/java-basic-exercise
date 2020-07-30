09ada4f54de0a9f5ab3389fa2e31f3c20945d5d4
xyz <xyzzhybz@163.com>
2020-07-30 20:46:46 +0800

    finished

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,38 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
-        //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
-
+        Scanner scanner = new Scanner(System.in);
+        String firstWordList = scanner.nextLine();
+        String secondWordList = scanner.nextLine();
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
-
+        result.stream().forEach(word-> System.out.println(word));
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+
+        validate(firstWordList);
+        validate(secondWordList);
+
+        List<String> firstList = Arrays.stream(firstWordList.split(","))
+                .map(word->word.toUpperCase())
+                .distinct()
+                .collect(Collectors.toList());
+        List<String> secondList = Arrays.stream(secondWordList.split(","))
+                .map(word->word.toUpperCase())
+                .distinct()
+                .collect(Collectors.toList());
+        return firstList.stream()
+                .filter(word->secondList.contains(word))
+                .collect(Collectors.toList());
+    }
+
+    public static void validate(String str){
+        if(str.contains(",,") || str.matches("[a-zA-Z,]+")){
+            throw new RuntimeException("input not valid");
+        }
     }
 }
